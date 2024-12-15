4a730e17727b05879474b1ad8a0fc898216d3f91
Dhirender pratap singh <division.pvt0@gmail.com>
2024-12-15 13:52:32 +0530

    feat: adding cors in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import connectDB from './utils/db.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -10,8 +12,13 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+const corsOptions = {
+    origin : 'http://localhost:5173/'
+}
 
+app.use(cors(corsOptions));
 
 app.get('/', (req,res,next)=>{
     res.send(`<h1>Hello World! </h1>`)
