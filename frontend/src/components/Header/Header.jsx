9569749d6c8c520b395ab6b099387edb3e2b97c5
Dhirender pratap singh <division.pvt0@gmail.com>
2024-12-28 16:18:33 +0530

    fix: header for dark mode

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Logo from './Logo'
-import NavELe from './NavELe'
-import NavBtns from './NavBtns'
+import React from 'react';
+import Logo from './Logo';
+import NavELe from './NavELe';
+import NavBtns from './NavBtns';
+
 const Header = () => {
-
-
   return (
     <>
-      <div className='pt-4 pb-2 px-12 flex justify-between border-b-2 border-gray-300 shadow-md'>
+      <div className='pt-4 pb-2 px-12 flex justify-between border-b-2 border-gray-300 dark:border-gray-700 shadow-md bg-white dark:bg-gray-900'>
         <Logo />
         <NavELe />
         <NavBtns />
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
